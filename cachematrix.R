6d04cdf54bc7ccd2cd484d59037d77a45d09b0a6
whaogood <haowang201314@gmail.com>
2014-11-23 17:46:44 +0200

    More comments supplied by whaogood

--- cachematrix.R
+++ cachematrix.R
@@ -11,22 +11,27 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setim <- function(invmat) im <<- invmat
   getim <- function() im
+  ## Make a list containing the four functions to be called by the function of cacheSolve
   list(set = set, get = get, setim = setim, getim = getim)  
 }
 
 
 
 ## The cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
-## If the inverse has already been calculated (and the matrix) has not changed), then the cachesolve should 
-## retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  ## Return a matrix that is the inverse of 'x'
+  
+  ## If the inverse has already been calculated (and the matrix) has not changed), then the cachesolve should 
+  ## retrieve the inverse from the cache.
   im <- x$getim()
   if(!is.null(im)) {
     message("getting cached data")
     return(im)
   }
+  
+  ## If the inverse has NOT been calculated, the a new calculation will be carried on using solve()
   data <- x$get()
   im <- solve(data)
   x$setim(im)
